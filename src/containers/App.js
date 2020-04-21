52ffbd819e02c41dbe1f219184b0d7e6ddf8e7c6
Randall Hammond <register@randallhammond.com>
2020-04-21 12:42:13 -0700

    Prettify

--- src/containers/App.js
+++ src/containers/App.js
@@ -32,8 +32,8 @@ class App extends Component {
     return !robots.length ? (
       <h1>Loading...</h1>
     ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
